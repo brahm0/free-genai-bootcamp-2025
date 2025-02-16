0540c46b010349885d0d22312fda94da03d0d98e
Brahm <99555050+brahm0@users.noreply.github.com>
2025-02-16 09:16:53 +0000

    Added code from @omenking to the backend-flask. Ed ited the code to reflect Russian language tutor

---
+++ lang-portal/backend-flask/sql/setup/create_table_words.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS words (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  Russian TEXT NOT NULL,
+  English TEXT NOT NULL,
+  Latin TEXT NOT NULL,
+  parts TEXT NOT NULL  -- Store parts as JSON string
+);
